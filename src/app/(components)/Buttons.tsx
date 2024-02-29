afdf604558add82cfab945a5de66f643c09a6b88
Alina Vetchanina <vetchanina.alina@gmail.com>
2024-02-29 16:51:39 +0100

    active button

--- src/app/(components)/Buttons.tsx
+++ src/app/(components)/Buttons.tsx
@@ -1,19 +1,33 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
 
 function Buttons() {
+  const pathname = usePathname();
+
   return (
     <div className="w-full flex  items-center justify-center gap-4 text-xs text-slate-400 md:text-sm md:gap-8">
       <Link className=" flex justify-center" href="/">
-        <button>🌱 Everyone 🌱</button>
+        <button className={` ${pathname === "/" ? "bg-purple-600/30" : ""}`}>
+          🌱 Everyone 🌱
+        </button>
       </Link>
 
       <Link className=" flex justify-center" href="/girls">
-        <button>🎀 Girls 🎀</button>
+        <button
+          className={` ${pathname === "/girls" ? "bg-fuchsia-500/30" : ""}`}
+        >
+          🎀 Girls 🎀
+        </button>
       </Link>
 
       <Link className=" flex justify-center" href="/boys">
-        <button>🍆 Boys 🍆</button>
+        <button
+          className={` ${pathname === "/boys" ? "bg-indigo-600/30" : ""}`}
+        >
+          🍆 Boys 🍆
+        </button>
       </Link>
     </div>
   );
